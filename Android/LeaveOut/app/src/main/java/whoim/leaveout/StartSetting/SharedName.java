7b173a160793a406e23fae3d76150b5924bfa2e3
powedonk123 <powedonk123@naver.com>
2017-05-14 23:43:19 +0900

    자동 로그인

---
+++ Android/LeaveOut/app/src/main/java/whoim/leaveout/StartSetting/SharedName.java
@@ -0,0 +1,12 @@
+package whoim.leaveout.StartSetting;
+
+/**
+ * SharedPreferences의 정보 이름
+ */
+
+public class SharedName {
+    /*
+    * 1. "id" = String
+    * */
+    public final static String SHARED_LOGIN_INFO = "login_info";        // 로그인 정보
+}
--- Android/LeaveOut/app/src/main/java/whoim/leaveout/login.java
+++ Android/LeaveOut/app/src/main/java/whoim/leaveout/login.java
@@ -1,12 +1,17 @@
 package whoim.leaveout;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -16,55 +21,106 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.tsengvn.typekit.TypekitContextWrapper;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Arrays;
 
+import whoim.leaveout.StartSetting.SharedName;
+import whoim.leaveout.User.UserInfo;
+
 // 로그인
 public class login extends AppCompatActivity {
 
-    private CallbackManager mCallbackManager;
-    private LoginButton mLoginButton;
+    private SharedPreferences mLoginShared;       // 상태 저장(로그인 정보)
+    private UserInfo mUserInfo;                 // 유저 정보
+    private CallbackManager mCallbackManager;   // 페이스북 전용 콜백 관리자
+    private LoginButton mLoginButton;           // 페이스북 로그인 버튼
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
 
-        mCallbackManager = CallbackManager.Factory.create();
+        mUserInfo = UserInfo.getInstance();     // 유저 정보 객체 가져오기
+        mLoginShared = getSharedPreferences(SharedName.SHARED_LOGIN_INFO, Activity.MODE_PRIVATE);     // 로그인 정보 상태
+
+        if (loginCheck()) {       // 자동 로그인 체크
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);        // 메인화면으로
+            startActivity(intent);
+//            finish();           // 액티비티 종료
+        }
+
         mLoginButton = (LoginButton) findViewById(R.id.facebook_loginBtn);
-        mLoginButton.setReadPermissions(Arrays.asList("public_profile", "email"));
-        mLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+        faceBookLoginSet();     // 페이스북 로그인 버튼 셋팅
+
+    }
+
+    // 페이스북 로그인 셋팅
+    private void faceBookLoginSet() {
+        mCallbackManager = CallbackManager.Factory.create();        // 페이스북 전용 콜백관리 생성
+        mLoginButton.setReadPermissions(Arrays.asList("public_profile", "email"));      // 페이스북 데이터 권한
+        mLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {       // 로그인 확인 콜백 메소드 생성
             @Override
-            public void onSuccess(final LoginResult loginResult) {
-                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
-                    @Override
-                    public void onCompleted(JSONObject object, GraphResponse response) {
-                        Log.d("result", object.toString());
-                        Log.d("response", response.toString());
-                    }
-                });
-
-                Bundle parameters = new Bundle();
-                parameters.putString("fields", "id,name,email,gender,picture");
-                graphRequest.setParameters(parameters);
-                graphRequest.executeAsync();
-
-                Log.d("TAG", loginResult.getAccessToken().getToken() + " [ID] " + loginResult.getAccessToken().getUserId());
+            public void onSuccess(final LoginResult loginResult) {      // 로그인 성공시
+                faceBookRequest(loginResult.getAccessToken());          // 페이스북에 사용자 정보 요청하기
+                Log.d("TAG", loginResult.getAccessToken().getToken());
             }
-
             @Override
-            public void onCancel() {
+            public void onCancel() {        // 로그인 취소시
                 Log.d("onCancel","취소");
 
             }
-
             @Override
-            public void onError(FacebookException error) {
+            public void onError(FacebookException error) {  // 로그인 실패시
                 Log.e("LoginErr",error.toString());
             }
         });
+    }
 
+    // 페이스북 데이터 정보 가져오기
+    private void faceBookRequest(AccessToken accessToken) {
+        GraphRequest graphRequest = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {        // 로그인
+            @Override
+            public void onCompleted(JSONObject data, GraphResponse response) {
+                try {
+                    Log.d("result", data.toString());
+                    mUserInfo.setFacebookId(data.getString("id"));      // 페이스북 유저 데이터 저장
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,email,gender,picture");     // 원하는 정보 파라미터로 넣기
+        graphRequest.setParameters(parameters);     // 셋팅
+        graphRequest.executeAsync();    // 페이스북으로 요청 전송
+    }
+
+    // 자동 로그인 체크
+    private boolean loginCheck() {
+        if(mLoginShared.getString("id",null) != null) {     // LeaveOut 로그인을 했을경우
+            Toast.makeText(this,"앱으로 로그인",Toast.LENGTH_SHORT).show();  // 테스트
+            mUserInfo.setId(mLoginShared.getString("id",null));       //  유저 id 셋팅
+            return true;
+        }
+        else if(AccessToken.getCurrentAccessToken() != null) {      // 페이스북 로그인을 했을경우
+            Toast.makeText(this,"페이스북으로 로그인",Toast.LENGTH_SHORT).show();  // 테스트
+            faceBookRequest(AccessToken.getCurrentAccessToken());       // 페이스북에 사용자 요청
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onResume() {
+/*        if (loginCheck()) {       // 자동 로그인 체크
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);        // 메인화면으로
+            startActivity(intent);
+//            finish();           // 액티비티 종료
+        }*/
+        super.onResume();
     }
 
     @Override
@@ -78,10 +134,28 @@ public class login extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), Join.class);
         startActivity(intent);
     }
-    // 메인화면으로 넘어가기
+
+    // 비번 찾기 화면
+    public void idPwSelect(View view) {
+        SharedPreferences.Editor LoginSharedEdit = mLoginShared.edit();     // 상태 저장 에디터
+        LoginSharedEdit.clear().commit();
+        Toast.makeText(this,"비번 찾기 버튼 테스트 완료",Toast.LENGTH_SHORT).show();
+    }
+
+    // 앱 로그인 버튼 메인화면으로 넘어가기(데이터베이스에서 확인 후 넘기기)
     public void loginButton(View v) {
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
+        EditText editText = (EditText) findViewById(R.id.userName);     // 유저 id 텍스트 필드
+        if(!editText.getText().toString().equals("")) {
+            SharedPreferences.Editor LoginSharedEdit = mLoginShared.edit();     // 상태 저장 에디터
+            LoginSharedEdit.putString("id",editText.getText().toString());      // 유저 id 상태 저장
+            LoginSharedEdit.commit();       // commit
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);        // 메인 화면으로
+            startActivity(intent);
+            finish();       // 액티비티 종료
+        }
+        else
+            Toast.makeText(this,"ID를 입력해주세여",Toast.LENGTH_SHORT).show();
+
     }
 
     // 메인화면으로 넘어가기
@@ -96,4 +170,6 @@ public class login extends AppCompatActivity {
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(TypekitContextWrapper.wrap(newBase));
     }
+
+
 }

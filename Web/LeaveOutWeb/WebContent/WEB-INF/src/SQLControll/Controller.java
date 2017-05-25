1590e30baa868ef6de8eab804a6560605a3a66fd
e <e>
2017-05-25 19:23:42 +0900

    cc

--- Web/LeaveOutWeb/WebContent/WEB-INF/src/SQLControll/Controller.java
+++ Web/LeaveOutWeb/WebContent/WEB-INF/src/SQLControll/Controller.java
@@ -13,6 +13,7 @@ import org.apache.jasper.compiler.ServletWriter;
 
 import javax.naming.*;
 
+@SuppressWarnings("serial")
 public class Controller extends HttpServlet {
 
 	DataSource ds;
@@ -20,6 +21,7 @@ public class Controller extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
 		response.setContentType("text/html; charset=euc-kr");
 		PrintWriter out = response.getWriter();
 		Connection conn = null;

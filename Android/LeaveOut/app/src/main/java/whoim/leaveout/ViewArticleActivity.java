61091daced4af62a591dc391e241b568bc6244b2
bu4566 <bu4566@naver.com>
2017-06-03 21:03:10 +0900

    댓글 동적으로 수정

--- Android/LeaveOut/app/src/main/java/whoim/leaveout/ViewArticleActivity.java
+++ Android/LeaveOut/app/src/main/java/whoim/leaveout/ViewArticleActivity.java
@@ -164,21 +164,25 @@ public class ViewArticleActivity extends AppCompatActivity
     }
 
     // 리스트뷰 펼처보기(한화면에)
+    // 리스트뷰 펼처보기(한화면에)
     public static void setListViewHeightBasedOnChildren(ListView listView) {
         ListAdapter listAdapter = listView.getAdapter();
         if (listAdapter == null) {
+            // pre-condition
             return;
         }
 
         int totalHeight = 0;
+
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.AT_MOST);
         for (int i = 0; i < listAdapter.getCount(); i++) {
             View listItem = listAdapter.getView(i, null, listView);
-            listItem.measure(0, 0);
+            listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
             totalHeight += listItem.getMeasuredHeight();
         }
-
         ViewGroup.LayoutParams params = listView.getLayoutParams();
-        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+
+        params.height = totalHeight;
         listView.setLayoutParams(params);
         listView.requestLayout();
     }

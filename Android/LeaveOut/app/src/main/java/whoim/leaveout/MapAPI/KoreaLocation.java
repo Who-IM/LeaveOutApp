1a32322a1ecdd04d48ee3c435edcda9c7342e36e
ckdtjr3689 <ckdtjr4467@gmail.com>
2017-07-18 11:38:25 +0900

    알림 설정

---
+++ Android/LeaveOut/app/src/main/java/whoim/leaveout/MapAPI/KoreaLocation.java
@@ -0,0 +1,14 @@
+package whoim.leaveout.MapAPI;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class KoreaLocation {
+    int deagu_count = 0;
+    LatLng seoul_left = new LatLng(37.5205240, 126.9151980);
+    LatLng seoul_right = new LatLng(37.6087670, 127.06489501);
+
+    LatLng deagu_left = new LatLng(35.7281660, 128.4433330);
+    LatLng deagu_right = new LatLng(35.934143035, 128.6940680);
+
+
+}
